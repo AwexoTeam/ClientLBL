d68947c251f5852969ebe830de806c6b9ff3106d
Awexo <AwexoTeam@gmail.com>
2019-12-28 00:21:12 +0100

    Updated navmesh. Need to fix error.

--- Assets/Scripts/Gameplay/Entities/Player.cs
+++ Assets/Scripts/Gameplay/Entities/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UMA.CharacterSystem;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,6 +8,7 @@ public partial class Player : MonoBehaviour
 {
     [Header("Gameplay Info")]
     public NavMeshAgent agent;
+    public DynamicCharacterAvatar avatar;
 
     public Vector3 targetPosition;
     private Vector3 lastTargetPosition;
@@ -23,40 +25,49 @@ public partial class Player : MonoBehaviour
     {
         get { return this == PlayerManager.instance.localPlayer; }
     }
-    
+
+    private void Start()
+    {
+        agent.enabled = true;
+    }
+
     private void Update()
     {
-            if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100))
             {
-                RaycastHit hit;
+                PlayerMovementRequest request =
+                    new PlayerMovementRequest(hit.point.x, hit.point.y, hit.point.z);
 
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                if (Physics.Raycast(ray, out hit, 100))
-                {
-                    PlayerMovementRequest request =
-                        new PlayerMovementRequest(hit.point.x, hit.point.y, hit.point.z);
-
-                    request.Serialize();
-                    request.Send();
-                }
-            }   
+                request.Serialize();
+                request.Send();
+            }
         }
 
-        //if (targetPosition != lastTargetPosition)
-        //{
-        //    agent.destination = targetPosition;
-        //    lastTargetPosition = targetPosition;
-        //}
+        if (targetPosition != lastTargetPosition)
+        {
+            agent.SetDestination(targetPosition);
+            lastTargetPosition = targetPosition;
+        }
 
         if (needUpdate)
         {
             //TODO: update uma data here.
+            string raceName = "";
+            raceName = bodyType == 0 ? "HumanMale" : raceName;
+            raceName = bodyType == 1 ? "HumanFemale" : raceName;
+            avatar.ChangeRace(raceName);
+            
             needUpdate = false;
         }
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawSphere(targetPosition, 2);   
+        Gizmos.DrawSphere(targetPosition, 0.3f);   
     }
 }
